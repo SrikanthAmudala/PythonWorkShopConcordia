40b51eb6beea7184b3eed6be3056e2aa08dc9709
Srikanth <srikanthamudala95@gmail.com>
2020-03-31 12:45:31 -0700

    Assignments for Week1

---
+++ Week1/Assignment_7.py
@@ -0,0 +1,8 @@
+"""
+Hand shake problem, find in the document
+
+"""
+
+#
+# def handshake(n):
+#     return n * (n - 1) // 2
---
+++ Week1/Assignment_8.py
@@ -0,0 +1,15 @@
+"""
+Operators,
+
+find the height of triangle, if base and area are given
+
+input: 2, 2
+output: 2
+
+input: 1000000, 1
+output: 1
+
+input: 17 100
+output: 12
+"""
+
